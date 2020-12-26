5b8017308ebaa1e6af961ae3f64a588a0117bc20
Junyoung, Sung <junyoung.sung@navercorp.com>
2020-12-26 19:20:31 +0900

    Initial commit

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 성준영 on 2020/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection: Tab = .featured
+
+    enum Tab {
+        case featured
+        case list
+    }
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem {
+                    Label("Featured", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}

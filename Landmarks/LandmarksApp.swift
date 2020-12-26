7595af09db26c85f076d6fdb182313a6860bea65
Junyoung, Sung <junyoung.sung@navercorp.com>
2020-12-26 16:35:33 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 성준영 on 2020/12/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
